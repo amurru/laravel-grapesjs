84dbd2bab412c73db303d6efefc32da15ce0f0dd
Amurru Zerouk <me@amurru.dev>
2025-11-21 16:11:16 +0300

    feat: make assets naming predictable

--- vite.config.js
+++ vite.config.js
@@ -5,40 +5,44 @@ import { viteStaticCopy } from 'vite-plugin-static-copy';
 export default defineConfig({
   plugins: [
     laravel({
-      input: [
-        'src/resources/js/index.js',
-        'src/resources/scss/gjs.scss'
-      ],
+      input: ['src/resources/js/index.js', 'src/resources/scss/gjs.scss'],
       buildDirectory: 'assets',
       // Ensure manifest is created for package asset resolution
-      manifest: true
+      manifest: true,
     }),
     viteStaticCopy({
       targets: [
         {
           src: 'node_modules/grapesjs/dist/fonts/*',
-          dest: 'fonts'
+          dest: 'fonts',
         },
         {
           src: 'src/resources/js/plugins/image-editor/svg/*',
-          dest: 'svg'
-        }
-      ]
-    })
+          dest: 'svg',
+        },
+      ],
+    }),
   ],
   build: {
     outDir: 'dist',
     emptyOutDir: true,
     sourcemap: false,
     // Ensure manifest is generated in root dist directory
-    manifest: 'manifest.json'
+    manifest: 'manifest.json',
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name].js',
+        chunkFileNames: 'assets/[name].js',
+        assetFileNames: 'assets/[name].[ext]',
+      },
+    },
   },
   css: {
     preprocessorOptions: {
       scss: {
         // Disable URL processing to match Mix behavior
-        api: 'modern-compiler'
-      }
-    }
-  }
+        api: 'modern-compiler',
+      },
+    },
+  },
 });
